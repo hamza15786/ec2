e4801b4791362c5bedc71a591804afc620080532
hamza15786 <43768836+hamza15786@users.noreply.github.com>
2022-07-24 20:27:31 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,9 +42,9 @@ variable "ami" {
   module "ec2-instance" {
   source  = "app.terraform.io/example-org-4f064f/ec2-instance/aws"
   version = "1.0.5"
- # region = "us-east-1"
-#  instance_type = "t2.micro"
-#   ami = "ami-0d5eff06f840b45e9"
+  region = var.region
+  instance_type = var.instance_type
+   ami = var.ami
 
     
   # insert required variables here
