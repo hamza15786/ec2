3866b75d925dd687fd51b4988ef9a1ac3ff28c62
hamza15786 <43768836+hamza15786@users.noreply.github.com>
2022-07-25 10:15:10 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
       version = "~> 3.27"
     }
   }
-    required_version = "~> 1.25"
+    required_version = "~> 0.15.4"
  # backend "remote" {
  #   hostname      = "app.terraform.io"
   #  organization  = "example-org-4f064f"
